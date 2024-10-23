c731b5bf95a81e3bb1985960b106b9c22e935dbe
gustin23 <agusmaseda05@gmail.com>
2024-10-23 15:53:06 -0300

    Primera versión del proyecto

--- src/main/java/com/example/javainscryption/Controllers/ActoController.java
+++ src/main/java/com/example/javainscryption/Controllers/ActoController.java
@@ -1,25 +1,41 @@
 package com.example.javainscryption.Controllers;
 
 import com.example.javainscryption.Entities.Acto;
+import com.example.javainscryption.Entities.Carta;
 import com.example.javainscryption.Repositories.ActoRepository;
+import com.example.javainscryption.Repositories.CartaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/actos")
 public class ActoController {
+
     @Autowired
     private ActoRepository actoRepository;
 
-    @PostMapping
-    public List<Acto> createActo(@RequestBody List<Acto> actos){
-        return actoRepository.saveAll(actos);
-    }
+    @Autowired
+    private CartaRepository cartaRepository;
 
     @GetMapping
-    public List<Acto> getAllProductos(){
+    public List<Acto> getAllActos() {
         return actoRepository.findAll();
+    }
+
+    // Asociar cartas a un acto existente
+    @PostMapping("/{id}/cartas")
+    public Acto addCartasToActo(@PathVariable Long id, @RequestBody List<Long> cartasIds) {
+        Optional<Acto> optionalActo = actoRepository.findById(id);
+        if (optionalActo.isPresent()) {
+            Acto acto = optionalActo.get();
+            List<Carta> cartas = cartaRepository.findAllById(cartasIds);
+            acto.getCartas().addAll(cartas);
+            return actoRepository.save(acto);
+        } else {
+            throw new RuntimeException("Acto no encontrado");
+        }
     }
 }
